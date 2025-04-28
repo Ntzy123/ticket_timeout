131ee568c9474a0464fae7dea23e779265b4ced0
Ntzy123 <cs16fox@vip.qq.com>
2025-04-28 23:55:09 +0800

    Update 2025.4.28 23:55

--- main.py
+++ main.py
@@ -29,19 +29,32 @@ class Ticket:
             self.json['endTime'] = f"{current_date} 23:59:59"
 
     # 子方法 20分钟超时提醒
-    def _timeout(self,target_time):
+    def _timeout(self, record, timeout_ticket):
         current_time = datetime.now()
-        target_time = datetime.strptime(target_time, "%Y-%m-%d %H:%M:%S")
+        target_time = datetime.strptime(record['feedBackTime'], "%Y-%m-%d %H:%M:%S")
         alert_time = target_time - timedelta(minutes=20)
         if current_time >= alert_time:
             # print("您有一条待处理的工单，任务即将超时请及时处理！")
-            pass
+            data = {
+                'workorderTitle': record.get('workorderTitle'),
+                'acceptName': record.get('acceptName'),
+                'feedBackTime': record.get('workorderTitle')
+            }
+            timeout_ticket['data'].append(data)
 
     # 20分钟超时提醒
     def query_timeout(self):
         for record in self.data['data']['records']:
-            timeout_ticket = {}
-            self._timeout(record['feedBackTime'])
+            timeout_ticket = {'data': []}
+            self._timeout(record, timeout_ticket)
+            timeout_ticket_num = len(timeout_ticket['data'])
+            for data in timeout_ticket['data']:
+                text = (f"任务描述：\t{record['workorderTitle']}\n接单人：\t{record['acceptName']}\n超时时间：\t{record['feedBackTime']}\n", "-" * 20, "\n")
+                #print("任务描述：\t", record['workorderTitle'])
+                #print("接单人：\t", record['acceptName'])
+                #print("超时时间：\t", record['feedBackTime'])
+                #print("\n", "-" * 20, "\n")
+                print(text)
 
     # 查询工单
     def query(self):
@@ -50,12 +63,13 @@ class Ticket:
         #查询
         for record in self.data['data']['records']:
             # 输出内容
+            """
             print("任务描述：\t", record['workorderTitle'])
             print("状态：\t\t", record['workorderStatusName'])
             print("接单人：\t", record['acceptName'])
             print("超时时间：\t", record['feedBackTime'])
             print("\n", "-" * 50, "\n")
-            
+            """
             # 导出已查询工单
             config = {"data": []}
             data = {
@@ -69,6 +83,7 @@ class Ticket:
         with open ("export.json", "w", encoding="utf-8") as file:
             json.dump(config, file, indent=4, ensure_ascii=False)
 
+# 循环查询
 def poll(tk):
     while True:
         time.sleep(300)
@@ -81,7 +96,7 @@ if __name__ == '__main__':
     t = threading.Thread(target=poll,args=(tk,))
     while True:
         tk.load(".config.json")
-        #tk.query_timeout()
+        tk.query_timeout()
         time.sleep(60)
     
     #pprint(tk.config)
