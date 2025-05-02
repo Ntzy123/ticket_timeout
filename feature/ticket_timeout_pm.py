3ed08a0fffd764275c86b12652777c15c84207f6
Ntzy123 <cs16fox@vip.qq.com>
2025-05-02 16:18:22 +0800

    update ticket_timeout_pm.py

--- feature/ticket_timeout_pm.py
+++ feature/ticket_timeout_pm.py
@@ -7,24 +7,29 @@ content = None
 lock = threading.Lock()
 
 # 循环查询
-def poll(tk):
+def loop_query(tk):
     while True:
         global content
         with lock:
             content = tk.query(time_range="today")
         print(content)
         time.sleep(300)
-
-def ticket_timeout_pm():
-    tk = Ticket()
-    tk.load(".config.json")
-    t = threading.Thread(target=poll, args=(tk,), daemon=True)
-    t.start()
-    while content != None:
-        time.sleep(1)
+        
+def loop_query_timeout(tk):
     while True:
         with lock:
             tk.load(".config.json")
             if content['msg'] == "success":
                 tk.query_timeout("pm")
         time.sleep(60)
+
+def ticket_timeout_pm():
+    tk = Ticket()
+    tk.load(".config.json")
+    t1 = threading.Thread(target=loop_query, args=(tk,), daemon=True)
+    t1.start()
+    t2 = threading.Thread(taget = loop_query_timeout, args=(tk,), daemon=True)
+    while content != None:
+        time.sleep(1)
+    t2.start()
+    
